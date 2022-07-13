9efb445b09dfa264b92f91d63ff34f8f8e52c272
sg <277276480@qq.com>
2022-07-13 11:49:41 +0800

    初始化

---
+++ src/settings.js
@@ -0,0 +1,17 @@
+module.exports = {
+
+  title: '人力资源管理后台',
+
+  /**
+   * @type {boolean} true | false
+   * @description Whether fix the header
+   * 设置appMain右侧主体上面的header是不是固定定位
+   */
+  fixedHeader: false,
+
+  /**
+   * @type {boolean} true | false
+   * @description Whether show the logo in sidebar
+   */
+  sidebarLogo: true
+}
